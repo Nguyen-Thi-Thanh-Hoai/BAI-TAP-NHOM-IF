07bfc7697bdf9803a57a92c7203f8101e0f83a31
Nguyen-Thi-Thanh-Hoai <146497398+Nguyen-Thi-Thanh-Hoai@users.noreply.github.com>
2023-11-10 20:48:13 +0700

    Add files via upload

--- Exercise40.py
+++ Exercise40.py
@@ -1,15 +1,3 @@
-# a=float(input("Nhap canh a="))
-# b=float(input("Nhap canh b="))
-# c=float(input("Nhap canh c="))
-# if ((a+b)>c and (b+c)>a and (c+a)>b) and (a>0 and b>0 and c>0):
-#    if a*a==(b*b)+(c*c) or b*b==(a*a)+(c*c) or c*c==(a*a)+(b*b):
-#      print('Tam giac vuong')
-#    elif a==b and b==c and c==a:
-#      print('Tam giac deu')
-#    else:
-#      print('Tam giac loai khac')  
-# else:
-#  print('So lieu nhap vao khong co tam giac nao tuong ung')  
 print("Nhap do dai cac canh cua tam giac: ")
 a = int(input("Canh 1: "))
 b = int(input("Canh 2: "))
@@ -24,4 +12,4 @@ if ((a+b)>c and (b+c)>a and (c+a)>b) and (a>0 and b>0 and c>0):
   else:
     print('Tam giac loai khac') 
 else:
-	print('TSo lieu nhap vao khong co tam giac nao tuong ung')
+	print('So lieu nhap vao khong co tam giac nao tuong ung')
