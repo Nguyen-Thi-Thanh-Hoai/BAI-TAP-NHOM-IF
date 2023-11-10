63d83614247c83a3b835ded7827d4508b6c2d6bd
Nguyen-Thi-Thanh-Hoai <146497398+Nguyen-Thi-Thanh-Hoai@users.noreply.github.com>
2023-11-10 20:00:47 +0700

    Add files via upload

---
+++ Exercise36.py
@@ -0,0 +1,7 @@
+chu_cai=(input("Nhap chu cai: "))
+if chu_cai =='u' or chu_cai =='e'or chu_cai =='o'or chu_cai =='a'or chu_cai =='i':
+    print(chu_cai,"la nguyen am")
+elif chu_cai =='y':
+    print(chu_cai,"vua la nguyen am vua la phu am")
+else:
+    print(chu_cai,"la phu am")
---
+++ Exercise37.py
@@ -0,0 +1,19 @@
+so_canh=(input("Nhap so canh: "))
+if so_canh==3:
+    print("Hinh tam giac")
+elif so_canh==4:
+    print("Hinh tu giac")
+elif so_canh==5:
+    print("Hinh ngu giac")
+elif so_canh==6:
+    print("Hinh luc giac")
+elif so_canh==7:
+    print("Hinh that giac")
+elif so_canh==8:
+    print("Hinh bat giac")
+elif so_canh==9:
+    print("Hinh cuu giac")
+elif so_canh==10:
+    print("Hinh thap giac")
+else:
+    print("So canh ban nhap khong thich hop")
