1d32333687b6ed3942bec5553484887c97a3cbd7
Nguyen-Thi-Thanh-Hoai <146497398+Nguyen-Thi-Thanh-Hoai@users.noreply.github.com>
2023-11-10 20:54:23 +0700

    Add files via upload

---
+++ Exercise38.py
@@ -0,0 +1,11 @@
+print("Danh sach thang: thang 1, thang 2, thang 3, thang 4, thang 5, thang 6, thang 7, thang 8, thang 9, thang 10, thang 11, thang 12")
+ten_thang=input('Nhap ten thang: ')
+if ten_thang=='thang 2':
+	print('So ngay: 28 hoac 29 ngay')	
+elif ten_thang=='thang 4' or ten_thang=='thang 6'or ten_thang=='thang 9' or ten_thang=='thang 11':
+	print('So ngay: 30 ngay')	
+elif ten_thang=='thang 1'or ten_thang=='thang 3'or ten_thang=='thang 5' or ten_thang=='thang 7' or ten_thang=='thang 8'or ten_thang=='thang 10'or ten_thang=='thang 12':
+	print('So ngay: 31 ngay')	
+else:
+	print('Gia tri ban nhap khong hop le') 
+	print('Vui long nhap lai')
