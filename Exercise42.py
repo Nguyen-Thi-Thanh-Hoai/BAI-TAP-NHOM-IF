54dc7ce9b488f9005db9b488ce390b8903bca5bf
Nguyen-Thi-Thanh-Hoai <146497398+Nguyen-Thi-Thanh-Hoai@users.noreply.github.com>
2023-11-11 14:59:48 +0700

    Add files via upload

--- Exercise42.py
+++ Exercise42.py
@@ -6,17 +6,17 @@ A4=440.00
 B4=493.88
 n=1
 tan_so=float(input("Nhap tan so = "))
-if C4-n <= tan_so <=C4+n:
+if C4-n<= tan_so <=C4+n:
     print("C4")
-elif D4-n <= tan_so <=D4+n:
+elif D4-n<= tan_so <=D4+n:
     print("D4")
-elif E4-n <= tan_so <=E4+n:
+elif E4-n<= tan_so <=E4+n:
     print("E4")
-elif F4-n <= tan_so <=F4+n:
+elif F4-n<= tan_so <=F4+n:
     print("F4")
-elif A4-n <= tan_so <=A4+n:
+elif A4-n<= tan_so <=A4+n:
     print("A4")
-elif B4-n <= tan_so <=B4+n:
+elif B4-n<= tan_so <=B4+n:
     print("B4")
 else:
     print("Tan so khong tuong ung voi not nhac")
